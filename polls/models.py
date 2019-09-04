1f2392c74ed5094d3c3877023c7a2c97d0d5354c
jereliu <jereliu@tencent.com>
2019-09-04 16:13:22 +0800

    1. dev:     参照教程，更新新的代码

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,9 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    # 通过related_name可以指定外键在另一个model中的引用名字， 默认为'foo_set'
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choice_set')
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+from django.template import loader
 
-
+'''
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 展示最近的5个投票问题
+    latest_question_list = Question.objects.order_by('-pub_time')[:10]
+
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question: %s." % question_id)
+    # 查找key为'question_id'的对象，找不到则raise 404异常
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+'''
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last ten published questions."""
+        return Question.objects.order_by('-pub_time')[:10]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
